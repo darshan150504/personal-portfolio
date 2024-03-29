daceba8b54f56b5ef463aeca2b0d669c3f0b6594
darshan-1505 <21ce019@charusat.edu.in>
2024-03-29 17:32:23 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,185 @@
+//My Bio
+export const Bio = {
+  name: "Darshan Chavda",
+  roles: ["Web Developer", "Engineering Student", "Piano Artist", "You-Tuber"],
+  description:
+    "Always want to learn Hello World in different ways. I am a Computer Engineering student from CHARUSAT. I interned at ProjectTree, focusing on JavaScript and Reactjs technologies. I love technology and find it amazing how it changes the world. Certain skills that I have acquired is Data Structures and Algorithms (Java) & Web Development (JavaScript, Reactjs). While I may not have all the skills, but I'm determined to learn and make up for it. Outside of tech, I am a Piano Artist. You can watch my skill on my youtube channel. Additionally I am good athlete.",
+  linkedin: "https://www.linkedin.com/in/darshan-chavda-9097a7245/",
+  x: "https://twitter.com/chavda_darshu",
+  resume:
+    "https://drive.google.com/drive/folders/1OM2t7n4oG6eP8060MegSxWPAULmpvUdk?usp=sharing",
+  insta: "https://www.instagram.com/darshan_1505/",
+  youtube: "https://youtube.com/@darshanchavda1505?si=BKmspeFneDBzAn0C",
+};
+
+//Skills
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMg5NJ37AdpWnunhxBuonZtXkHhscGP3Z5Wyv1k5xMUg&s",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Language Knowledge",
+    skills: [
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/695px-C_Programming_Language.svg.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSKJCNaNUeMFBN-OQijlZbhGt1vwGwLw0xI_Xqjyd_VA&s",
+      },
+      {
+        name: "Angular",
+        image:
+          "https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+//Exprriences
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D560BAQHiASmDUNFNdw/company-logo_200_200/0/1695364494089/pttech_logo?e=2147483647&v=beta&t=Q5nHYQF9pE7gjxfsfuopUio00a_K_LCdq4jwMSHjTSc",
+    role: "Frontend Engineer Intern",
+    company: "ProjectTree",
+    date: "May 2023 - June 2023",
+    desc: "Studied React and JavaScript. Create a project with the React and JavaScript libraries. Acquired understanding of industry procedures and client interactions. Learn how to lead and collaborate with a team.",
+    skills: ["JavaScript", "ReactJS", "HTML", "CSS"],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  },
+];
+
+//Education
+export const education = [
+  {
+    id: 0,
+    img: "https://secure.gravatar.com/blavatar/fc6bfd97816e5ad165a27436a4bcdbf14bda0b165a4f93da7c59a581e1b5dfd8?s=200&ts=1707226783",
+    school: "Chandubhai S. Patel Institute of Technology (CHARUSAT)",
+    date: "Oct 2021 - Present",
+    grade: "8.5 CGPA (until 5th Semester)",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Engineering at Charotar University of Science and Technology, Anand. I have completed 5 semesters and have a CGPA of 8.5. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems among others.",
+    degree: "Bachelor of Technology - BTech, Computer Engineering",
+  },
+  {
+    id: 1,
+    img: "https://data.zibma.com/Campus/3/2023/02/W800orH800/7cb5411b-e0bb-4c6a-a175-36fbae1e7e37.png",
+    school: "Sarvodaya High School, Ankleshwar",
+    date: "June 2017 - June 2019",
+    grade: "84%",
+    desc: "I completed my high school education (class 12) at Sarvodaya High School, Ankleshwar.",
+    degree: "HSC",
+  },
+  {
+    id: 2,
+    img: "http://sanskardeep.org.in/images/SanskardeepLogoFinal.jpg",
+    school: "SMT. P. D. Shroff Sanskardeep Vidhyalaya, Ankleshwar",
+    date: "Until April 2017",
+    grade: "82.3%",
+    desc: "I completed my secondary school education (class 10) at SMT. P. D. Shroff Sanskardeep Vidhyalaya, Ankleshwar.",
+    degree: "SSC",
+  },
+];
+
+// Projects
+export const projects = [
+  {
+    id: 1,
+    title: "Portfolio",
+    date: "March 2024",
+    description:
+      "It give the information about me and show case my skills as well as",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/dc/Portfolio.hu_full_logo.png",
+    tags: [
+      "React Js",
+      "JavaScript",
+      "CSS"
+    ],
+    github: "",
+    deploylink: ""
+  },
+];
